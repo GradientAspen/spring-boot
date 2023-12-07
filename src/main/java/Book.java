e576c08db2670089316922a7d9afaea1b5c33221
testSouth <southparaplan@gmail.com>
2023-12-07 22:57:24 +0200

    secondCommit

---
+++ src/main/java/Book.java
@@ -0,0 +1,35 @@
+public class Book {
+    Long id;
+    String title;
+    int price;
+
+    public Book(Long id, String title, int price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
